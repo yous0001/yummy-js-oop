e9c51a915c81c3b4919590250b65aea76c0c3062
Yousef emad mikhail <mad33833@gmail.com>
2024-08-06 14:19:02 +0300

    add: display  meal details

--- js/details.js
+++ js/details.js
@@ -1,15 +1,19 @@
+import { displayMealDetials } from "./ui.js"
+
 export class MealDetails{
     constructor(id){
         this.id=id
         this.getMealDetails(id)
-        
         
     }
     async getMealDetails(id){
         const URL=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
         let responce=await fetch(URL)
         let data=await responce.json()
-        console.log(data);
+
+        
+        displayMealDetials(data.meals[0]);
+        
 
     }
 }
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,4 @@
 
-
 export function displayMeals(meals){
     let cartona=``;
     meals.forEach(meal => {
@@ -15,4 +14,67 @@ export function displayMeals(meals){
         `        
     });
     document.getElementById("row").innerHTML=cartona
+}
+
+export function displayMealDetials(meal){
+    let box=``
+    let Ingredient=[]
+    let measure=[]
+    let counter=0
+    let mealMap=new Map(Object.entries(meal))
+    let recipe=``
+    let tagsHTMl=``
+
+    for (let [key, value] of mealMap) {
+        counter++
+        if(counter>=10&&counter<30){
+            if(value==="")
+                continue;
+            Ingredient.push(value)
+        }
+        if(counter>=30&&counter<50){
+            if(value===" ")
+                break;
+            measure.push(value)
+        }
+    }
+    for(let i=0;i<Ingredient.length;i++){
+        recipe+=`
+        <li class="alert alert-info m-2 p-1">${measure[i]} ${Ingredient[i]}</li>`
+    }
+    if(meal.strTags!=null){
+        let tags=(meal.strTags).split(',')
+
+        tags.forEach((tag)=>{
+            tagsHTMl+=`<li class="alert alert-danger m-2 p-1">${tag}</li>`
+        })
+    }
+    
+
+
+    let cartona=`
+    <div class="col-md-4 text-white">
+                <img src="${meal.strMealThumb}" class="w-100">
+                <h2>${meal.strMeal}</h2>
+            </div>
+            <div class="col-md-8 text-white">
+                <h2>Instructions</h2>
+                <p>${meal.strInstructions}</p>
+                <h3><span class="fw-bolder">Area : </span>${meal.strArea}</h3>
+                <h3><span class="fw-bolder">Category : </span>${meal.strCategory}</h3>
+                <h3>Recipes :</h3>
+                <ul class="list-unstyled d-flex g-3 flex-wrap">
+                    ${recipe}
+                </ul>
+
+                <h3>Tags :</h3>
+                <ul class="list-unstyled d-flex g-3 flex-wrap">
+                    ${tagsHTMl}
+                </ul>
+
+                <a target="_blank" href="${meal.strSource}"
+                    class="btn btn-success">Source</a>
+                <a target="_blank" href="${meal.strYoutube}" class="btn btn-danger">Youtube</a>
+            </div>`
+            document.getElementById("row").innerHTML=cartona
 }
